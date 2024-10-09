05095927b02e1688369e28d01f5ebf1752163360
Brad Hilton <brad.hilton.nw@gmail.com>
2024-10-09 22:28:53 +0000

    chore: Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-sudo snap install --classic code astral-uv
-code --install-extension ms-python.python
-code --install-extension ms-python.python
-code --install-extension ms-python.black-formatter
-code --install-extension ms-toolsai.jupyter
-uv sync
+(
+    cursor --install-extension ms-python.python &
+    cursor --install-extension ms-python.black-formatter &
+    cursor --install-extension ms-toolsai.jupyter &
+    cursor --install-extension github.copilot
+) & (
+    sudo snap install --classic astral-uv &&
+    uv sync
+) & (
+    git config --global user.name "Brad Hilton" && 
+    git config --global user.email "brad.hilton.nw@gmail.com"
+) & wait # Wait for all background processes to finish
