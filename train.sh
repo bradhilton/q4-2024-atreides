9b9116fec063200d8c5f23eda645a5c7a5a145f8
Brad Hilton <brad.hilton.nw@gmail.com>
2024-10-12 00:04:40 +0000

    fix: distributed training support

--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 jupyter nbconvert experiments/02-llama3.ipynb --to python --TagRemovePreprocessor.remove_cell_tags='["magic"]'
-uv run torchrun --standalone --nproc_per_node=1 experiments/02-llama3.py
+uv run torchrun --standalone --nproc_per_node=$(nvidia-smi -L | wc -l) experiments/02-llama3.py
