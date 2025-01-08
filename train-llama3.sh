ead7c162af3cefd98ee7850abc61186c1cbea5a5
Brad Hilton <brad.hilton.nw@gmail.com>
2025-01-08 22:10:33 +0000

    chore: Update llama3 training

--- train-llama3.sh
+++ train-llama3.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-jupyter nbconvert experiments/02-llama3.ipynb --to python --TagRemovePreprocessor.remove_cell_tags='["magic"]'
-uv run torchrun --standalone --nproc_per_node=$(nvidia-smi -L | wc -l) experiments/02-llama3.py
+
+if [ -z "$1" ]; then
+    echo "Please provide a name argument"
+    exit 1
+fi
+
+NAME=$1
+
+jupyter nbconvert experiments/002-llama3.ipynb --to python --TagRemovePreprocessor.remove_cell_tags='["magic"]'
+uv run torchrun --standalone --nproc_per_node=$(nvidia-smi -L | wc -l) experiments/002-llama3.py --name $NAME
